87f084c0eb7579daec82a92a935271be503641fb
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-04-16 14:45:28 +0600

    authApi and authSlice setup for register and login

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,8 +1,73 @@
 import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
 
 export const authApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => {
+    endpoints: (builder) => ({
         // endpoints here
+        register: builder.mutation({
+            query: (data) => ({
+                url: '/register',
+                method: 'POST',
+                body: data,
+            }),
 
-    }
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                try {
+                    const result = await queryFulfilled;
+
+                    localStorage.setItem(
+                        "auth",
+                        JSON.stringify({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                        })
+                    );
+
+                    dispatch(
+                        userLoggedIn({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                        })
+                    );
+                } catch (err) {
+                    //do not nothing...
+                    console.log('error');
+                }
+            },
+
+        }),
+        login: builder.mutation({
+            query: (data) => ({
+                url: '/login',
+                method: 'POST',
+                body: data,
+            }),
+
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                try {
+                    const result = await queryFulfilled;
+
+                    localStorage.setItem(
+                        "auth",
+                        JSON.stringify({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                        })
+                    );
+
+                    dispatch(
+                        userLoggedIn({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                        })
+                    );
+                } catch (err) {
+                    //do not nothing...
+                    console.log('error');
+                }
+            },
+        }),
+    }),
 });
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
