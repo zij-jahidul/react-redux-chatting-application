9a9483e3075d7dcbb6c230794b4fda025c0f3efa
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-04-16 16:49:49 +0600

    Public and Private route setup and logout handle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import useAuthCheck from "./hooks/useAuthCheck";
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
     const authChecked = useAuthCheck();
@@ -11,10 +13,48 @@ function App() {
     return !authChecked ? <>Checking Authentication...</> : (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={<Conversation />} />
-                <Route path="/inbox/:id" element={<Inbox />} />
+
+                <Route path="/"
+                    element={
+                        <>
+                            <PublicRoute>
+                                <Login />
+                            </PublicRoute>
+                        </>
+                    }
+                />
+
+                <Route path="/register"
+                    element={
+                        <>
+                            <PublicRoute>
+                                <Register />
+                            </PublicRoute>
+                        </>
+                    }
+                />
+
+                <Route path="/inbox"
+                    element={
+                        <>
+                            <PrivateRoute>
+                                <Conversation />
+                            </PrivateRoute>
+                        </>
+
+                    }
+                />
+
+                <Route path="/inbox/:id"
+                    element={
+                        <>
+                            <PrivateRoute>
+                                <Inbox />
+                            </PrivateRoute>
+                        </>
+                    }
+                />
+
             </Routes>
         </Router>
     );
