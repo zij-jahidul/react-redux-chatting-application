8da9d2adefd1a2b61850735a58719a7a6c5b77fa
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-04-17 00:39:41 +0600

    Conversation Functionality build

--- src/components/inbox/ChatIItems.js
+++ src/components/inbox/ChatIItems.js
@@ -1,28 +1,49 @@
+import { useSelector } from "react-redux";
+import { useGetConversationsQuery } from "../../features/conversations/conversationsApi";
 import ChatItem from "./ChatItem";
+import Error from '../ui/Error';
+import moment from "moment/moment";
+import getPartnerInfo from "../../utils/getPartnerInfo";
+import gravatarUrl from "gravatar-url";
+import { Link } from "react-router-dom";
 
 export default function ChatItems() {
-    return (
-        <ul>
-            <li>
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Saad Hasan"
-                    lastMessage="bye"
-                    lastTime="25 minutes"
-                />
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Sumit Saha"
-                    lastMessage="will talk to you later"
-                    lastTime="10 minutes"
-                />
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Mehedi Hasan"
-                    lastMessage="thanks for your support"
-                    lastTime="15 minutes"
-                />
-            </li>
-        </ul>
-    );
+    const { user } = useSelector(state => state.auth) || {};
+    const { email } = user || {};
+    const { data: conversations, isLoading, isError, error } = useGetConversationsQuery(email);
+
+    // decide what to render
+    let content = null;
+    if (isLoading) {
+        content = (<li className="m-2 text-center">Loading...</li>);
+    } else if (!isLoading && isError) {
+        content = (<li className="m-2 text-center"><Error message={error?.data.message} /></li>);
+    } else if (!isLoading && !isError && conversations?.length === 0) {
+        content = (<li className="m-2 text-center">No Conversation found!</li>);
+    } else if (!isLoading && !isError && conversations?.length > 0) {
+        content = conversations.map((conversation) => {
+            const { id, message, timestamp } = conversation;
+            const { email } = user || {};
+            const { name, email: partnerEmail } = getPartnerInfo(conversation.users, email);
+
+            return (
+                <li key={conversation.id}>
+                    <Link to={`/inbox/${id}`}>
+                        <ChatItem
+                            avatar={gravatarUrl(partnerEmail, {
+                                size: 80
+                            })}
+                            name={name}
+                            lastMessage={message}
+                            lastTime={moment(timestamp).fromNow()}
+                        />
+                    </Link>
+
+                </li>
+
+            )
+        });
+    }
+
+    return <ul> {content} </ul>;
 }
